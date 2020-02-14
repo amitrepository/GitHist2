bclozel/sagan

Add new Sagan client application
This commit brings the new JavaScript files and static resources for the
client application.

Everything is bundled and optimized with a webpack build. The Gradle
build is triggering the NPM toolchain and packaging the sagan-client
module as a JAR.

sagan-client/webpack.config.js
@@ -1,0 +1,81 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/app/main.js',
+        guide: './src/app/guide.js',
+        team: './src/app/team.js',
+        blog: './src/app/blog.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'build', 'dist'),
+        filename: 'js/[name].js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: "[id].css"
+        }),
+        new CopyPlugin([
+            {from: './src/images', to: 'images'}
+        ]),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            'window.jQuery': 'jquery',
+            'window.$': 'jquery'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {publicPath: '../'},
+                    },
+                    'css-loader',
+                ]
+            },
+            {
+                test: /.*\/webfonts\/.*$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'css/[name].[ext]',
+                },
+            },
+
+            {
+                test: /.*\/fonts\/.*/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]',
+                },
+            },
+            {
+                test: /^images/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                },
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        runtimeChunk: false,
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({}),
+            new TerserPlugin()
+        ]
+    }
+};

amitrepository/GitHist2

webpack.config.js
@@ -1,0 +1,84 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: './src/app/main.js',
+        guide: './src/app/guide.js',
+        project: './src/app/project.js',
+        team: './src/app/team.js',
+        profile: './src/app/profile.js',
+        blog: './src/app/blog.js',
+        admin: './src/app/admin.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'build', 'dist'),
+        filename: 'js/[name].js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: "[id].css"
+        }),
+        new CopyPlugin([
+            {from: './src/images', to: 'images'}
+        ]),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            'window.jQuery': 'jquery',
+            'window.$': 'jquery'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {publicPath: '../'},
+                    },
+                    'css-loader',
+                ]
+            },
+            {
+                test: /.*\/webfonts\/.*$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'css/[name].[ext]',
+                },
+            },
+
+            {
+                test: /.*\/fonts\/.*/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]',
+                },
+            },
+            {
+                test: /^images/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                },
+            }
+        ]
+    },
+    optimization: {
+        minimize: true,
+        runtimeChunk: false,
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({}),
+            new TerserPlugin()
+        ]
+    }
+};